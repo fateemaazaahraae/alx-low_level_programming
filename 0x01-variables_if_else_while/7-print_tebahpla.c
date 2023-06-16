a3e472f5d55352faa0d6e33d3817339343bd0b27
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-06-16 09:10:00 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char character;
+for (character = 'z'; character <= 'a'; character--)
+{
+putchar(character);
+}
+putchar('\n');
+return (0);
+}
