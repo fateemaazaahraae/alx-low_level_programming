4c4a1ab1f5cad7c189616c70e10131672a7a3fac
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-06-16 08:04:45 -0700

    hi

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - Entry point 
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Prints the last digit of a randomly
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int n;
