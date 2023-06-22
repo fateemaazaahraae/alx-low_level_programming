5bd9d9a973b972b055eea63ba0b00bc0b3a479ef
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-06-22 12:09:35 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+int i = 0;
+while (i <= 9)
+{
+int j = 0;
+while (j <= 14)
+{ 
+if (j >= 10)
+{
+_putchar('1');
+_putchar(j % 10 + '0');
+}
+else
+{
+_putchar(j + '0');
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
