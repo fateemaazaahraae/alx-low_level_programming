f5cfa3a955f979b067c9723dd55976496e17bfd5
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-06-22 14:14:47 -0700

    hi

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,23 @@ while (i <= 100)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf(" Fizz");
+}
+else if (i == 1)
+{
+printf("%i", i);
 }
 else
 {
-printf("%i ", i);
+printf(" %i", i);
 }
 i++;
 }
