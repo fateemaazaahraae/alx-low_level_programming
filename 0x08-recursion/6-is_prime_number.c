7b3b24971bf3a4dafe32170ab1844208a7b691d0
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-10-26 08:29:04 -0700

    primenumber

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int primenum(int num);
+int primenum(int n, int i);
 
 /**
  * is_prime_number - check if the number is prime or not
@@ -14,7 +14,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (primenum(n));
+	return (primenum(n, n - 1));
 }
 
 /**
@@ -25,15 +25,11 @@ int is_prime_number(int n)
  * Return: 1 or 0
  */
 
-int primenum(int num)
+int primenum(int n, int i)
 {
-	int i;
-
-	i = 2;
-	while (i <= num / 2)
-	{
-		if (num % i == 0)
-			return (0);
-	}
+	if (i == 1)
 	return (1);
-}
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (primenum(n, i - 1));
+}  
