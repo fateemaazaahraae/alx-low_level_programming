e9ffb2677d22ebe3c2b4d0a9712cbb07ae274045
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-10-27 08:28:43 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry of the program
+ * @ac: number of command line arguments
+ * @av: command line arguments
+ * Return: always 0 (success)
+ */
+
+int main(int ac, char *av[])
+{
+	if (ac == 3)
+	{
+		int result;
+
+		result = atoi(av[1]) * atoi(av[2]);
+		printf("%i\n", result);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
