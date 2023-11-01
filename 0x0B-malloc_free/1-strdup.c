0d0de38954b99673c194a079e55d0a6c48bee2d1
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-01 07:59:14 -0700

    checking the task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - allocate memory for a string and copy it
+ * @str: string
+ * Return:string
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len])
+		len++;
+	ptr = malloc(sizeof(char) * len);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[len] = '\0';
+	return (ptr);
+}
