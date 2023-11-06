17eae38ead7ba5980dcef97c5737c9f3007d290c
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-06 04:02:33 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog{
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: just a struct dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
