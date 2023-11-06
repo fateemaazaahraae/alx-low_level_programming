d00f715d6204dc3840f4814e4448fceafd0bacf9
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2023-11-06 04:14:22 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - assign value to name, age and owner
+ * @d: struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
